6fc3e42ebcd0dd1fbc04c6be62cac3ee331e4b50
Arthur Urlic <aurlic@student.42.fr>
2024-06-03 10:53:22 +0200

    exercise structure

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aurlic <aurlic@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 13:20:14 by aurlic            #+#    #+#             */
+/*   Updated: 2024/05/31 13:34:17 by aurlic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iomanip>
+#include <string>
+
+class ScalarConverter
+{
+	private:
+		
+
+	public:
+		ScalarConverter();
+		~ScalarConverter();
+};
