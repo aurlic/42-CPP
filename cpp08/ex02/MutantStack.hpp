109e7f867a86a1e0a06d862b4bea1714fd982d6e
Arthur Urlic <aurlic@student.42.fr>
2024-06-21 10:55:35 +0200

    improved main, removed const iterators, further understood

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: aurlic <aurlic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 15:02:50 by aurlic            #+#    #+#             */
-/*   Updated: 2024/06/17 17:09:54 by aurlic           ###   ########.fr       */
+/*   Updated: 2024/06/21 10:21:12 by aurlic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ class MutantStack : public std::stack<T, Container>
 {
 	public:
 		typedef typename Container::iterator iterator;
-		typedef typename Container::const_iterator const_iterator;
 		typedef typename Container::reverse_iterator reverse_iterator;
-		typedef typename Container::const_reverse_iterator const_reverse_iterator;
 
 		MutantStack() : std::stack<T, Container>() {}
 		~MutantStack() {}
@@ -35,11 +33,7 @@ class MutantStack : public std::stack<T, Container>
 
 		// Iterator functions :
 		iterator				begin()			{ return (this->c.begin()); }
-		const_iterator			begin() const	{ return (this->c.begin()); }
 		iterator				end()			{ return (this->c.end()); }
-		const_iterator			end() const		{ return (this->c.end()); }
 		reverse_iterator		rbegin() 		{ return (this->c.rbegin()); }
-		const_reverse_iterator	rbegin() const	{ return (this->c.rbegin()); }
 		reverse_iterator		rend()			{ return (this->c.rend()); }
-		const_reverse_iterator	rend() const	{ return (this->c.rend()); }
 };
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,42 +6,92 @@
 /*   By: aurlic <aurlic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 17:07:47 by aurlic            #+#    #+#             */
-/*   Updated: 2024/06/17 17:09:12 by aurlic           ###   ########.fr       */
+/*   Updated: 2024/06/21 10:55:11 by aurlic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-int main()
-{
+int main() {
 	MutantStack<int> mstack;
 
-	mstack.push(5);
-	mstack.push(17);
-
-	std::cout << mstack.top() << std::endl;
-	
-	mstack.pop();
-	
-	std::cout << mstack.size() << std::endl;
-	
+	mstack.push(1);
+	mstack.push(2);
 	mstack.push(3);
+	mstack.push(4);
 	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	
-	while (it != ite)
-	{
-		std::cout << *it << std::endl;
-		++it;
+
+	/* ************************************************************************** */
+	std::cout << "*------------------------------*" << std::endl;
+	std::cout << "| pop, top, size               |" << std::endl;
+	std::cout << "*------------------------------*" << std::endl;
+	std::cout << "| Top element: " << mstack.top() << "               |" << std::endl;
+	std::cout << "| Stack size: " << mstack.size() << "                |" << std::endl;
+
+	mstack.pop();
+
+	std::cout << "| Top element after pop: " << mstack.top() << "     |" << std::endl;
+	std::cout << "| Stack size after pop: " << mstack.size() << "      |" << std::endl;
+	std::cout << "*------------------------------*" << std::endl;
+	/* ************************************************************************** */
+
+	std::cout << "      ~~~      " << std::endl;
+
+	/* ************************************************************************** */
+	std::cout << "*--------------------------------------------*" << std::endl;
+	std::cout << "| listing elements                           |" << std::endl;
+	std::cout << "*--------------------------------------------*" << std::endl;
+	std::cout << "| Stack elements (iterator): ";
+	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it) {
+		std::cout << *it << " ";
 	}
-	std::stack<int> s(mstack);
+	std::cout << "        |"<< std::endl;
+
+	std::cout << "| Stack elements (reverse iterator): ";
+	for (MutantStack<int>::reverse_iterator rit = mstack.rbegin(); rit != mstack.rend(); ++rit) {
+		std::cout << *rit << " ";
+	}
+	std::cout << "|" << std::endl;
+	std::cout << "*--------------------------------------------*" << std::endl;
+	/* ************************************************************************** */
+
+	std::cout << "      ~~~      " << std::endl;
+
+	/* ************************************************************************** */
+	std::cout << "*-------------------------------------------------------*" << std::endl;
+	std::cout << "| incrementing elements                                 |" << std::endl;
+	std::cout << "*-------------------------------------------------------*" << std::endl;
+	std::cout << "| Incrementing each element by 1 using iterator.        |" << std::endl;
+	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it) {
+		*it += 1;
+	}
+	std::cout << "| Stack elements after incrementing (iterator): ";
+	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << "|"<< std::endl;
+	
+	std::cout << "| Stack elements (reverse iterator): ";
+	for (MutantStack<int>::reverse_iterator rit = mstack.rbegin(); rit != mstack.rend(); ++rit) {
+		std::cout << *rit << " ";
+	}
+	std::cout << "           |" << std::endl;
+	std::cout << "*-------------------------------------------------------*" << std::endl;
+	/* ************************************************************************** */
+
+	std::cout << "      ~~~      " << std::endl;
+
+	/* ************************************************************************** */
+	MutantStack<int> emptyStack;
+	std::cout << "*-------------------------------------*" << std::endl;
+	std::cout << "| Iterating over an empty stack:      |";
+	for (MutantStack<int>::iterator it = emptyStack.begin(); it != emptyStack.end(); ++it) {
+		std::cout << *it << " "; // Should not output anything
+	}
+	std::cout << std::endl << "| Done (no output expected)           |" << std::endl;
+	std::cout << "*-------------------------------------*" << std::endl;
+
+	/* ************************************************************************** */
 	
 	return 0;
 }
